ea2c509d617079dd39a62579935a584c208093c6
udonyx <usamani@onyxcapitaltechnology.com>
2024-11-18 11:46:51 +0000

    v2 websocket client (#21)

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,18 @@ from typing import AsyncIterator
 import pytest
 
 from onyx_otc.websocket import OnyxWebsocketClient
-from tests.utils import OnResponse
+from onyx_otc.websocket_v2 import OnyxWebsocketClientV2
+from tests.utils import OnResponse, OnResponseV2
 
 
 @pytest.fixture
 def responses() -> OnResponse:
     return OnResponse()
+
+
+@pytest.fixture
+def responsesv2() -> OnResponseV2:
+    return OnResponseV2()
 
 
 @pytest.fixture
@@ -42,6 +48,25 @@ async def cli(responses: OnResponse):
     read_task = asyncio.create_task(cli.run())
     # await for authentication
     await responses.get_response()
+    try:
+        yield cli
+    finally:
+        read_task.cancel()
+        try:
+            await read_task
+        except asyncio.CancelledError:
+            pass
+
+
+@pytest.fixture
+async def cliv2(responsesv2: OnResponseV2):
+    cli = OnyxWebsocketClientV2(
+        on_response=responsesv2.on_response,
+        on_event=responsesv2.on_event,
+    )
+    assert cli.api_token
+    read_task = asyncio.create_task(cli.connect())
+    await responsesv2.get_otc_response()
     try:
         yield cli
     finally:
