c78767f39791343fa0751125e747ef123d0ed179
lsbardel <lsbardella@onyxcapitaltechnology.com>
2025-05-01 12:21:55 +0100

    Better annotation for Timestamp

--- tests/v2/test_protos.py
+++ tests/v2/test_protos.py
@@ -1,6 +1,27 @@
-from onyx_otc.v2 import requests_pb2, responses_pb2
+from datetime import datetime, timedelta, timezone
+
+from pydantic import TypeAdapter
+
+from onyx_otc.responses import LiveWebsocket
 
 
-def test_protos() -> None:
-    assert requests_pb2.OtcRequest is not None
-    assert responses_pb2.OtcResponse is not None
+def test_conversions() -> None:
+    now = datetime.now(timezone.utc)
+    started = now - timedelta(seconds=1)
+    lv = LiveWebsocket(started=started, current_time=now)  # type: ignore[arg-type]
+    assert lv.started
+    assert lv.started.to_datetime() == started
+    assert lv.age == timedelta(seconds=1)
+    lv = LiveWebsocket(started=started.isoformat(), current_time=now)  # type: ignore[arg-type]
+    assert lv.started
+    assert lv.started.to_datetime() == started
+    assert lv.age == timedelta(seconds=1)
+
+
+def test_json_schema() -> None:
+    schema = TypeAdapter(LiveWebsocket).json_schema()
+    assert schema
+    started = schema["properties"]["started"]
+    assert started == dict(
+        type="string", title="Started", format="date-time", default=0
+    )
